2065f68275793b97c22b7ee12f9db55c73039527
Peiman Timaji <peimantimaji@yahoo.com>
2025-04-20 17:58:36 +0100

    interface comments

--- src/main/java/com/teamoneboxoffice/interfaces/JARServiceImpl/operations/IEvent.java
+++ src/main/java/com/teamoneboxoffice/interfaces/JARServiceImpl/operations/IEvent.java
@@ -3,34 +3,135 @@ package com.teamoneboxoffice.interfaces.JARServiceImpl.operations;
 import java.sql.Date;
 import java.sql.Time;
 
+/**
+ * Interface representing an event with basic event details and venue information.
+ */
 public interface IEvent {
 
-    // Getters and setters for each attribute
+    /**
+     * Gets the unique identifier for the event.
+     *
+     * @return The event ID.
+     */
     int getEventID();
+
+    /**
+     * Sets the unique identifier for the event.
+     *
+     * @param eventID The event ID to set.
+     */
     void setEventID(int eventID);
 
+    /**
+     * Gets the name of the event.
+     *
+     * @return The event name.
+     */
     String getEventName();
+
+    /**
+     * Sets the name of the event.
+     *
+     * @param eventName The event name to set.
+     */
     void setEventName(String eventName);
 
+    /**
+     * Gets the type of the event.
+     *
+     * @return The event type.
+     */
     String getEventType();
+
+    /**
+     * Sets the type of the event.
+     *
+     * @param eventType The event type to set.
+     */
     void setEventType(String eventType);
 
+    /**
+     * Gets the date of the event.
+     *
+     * @return The event date.
+     */
     Date getEventDate();
+
+    /**
+     * Sets the date of the event.
+     *
+     * @param eventDate The event date to set.
+     */
     void setEventDate(Date eventDate);
 
+    /**
+     * Gets the start time of the event.
+     *
+     * @return The event start time.
+     */
     Time getEventStartTime();
+
+    /**
+     * Sets the start time of the event.
+     *
+     * @param eventStartTime The event start time to set.
+     */
     void setEventStartTime(Time eventStartTime);
 
+    /**
+     * Gets the end time of the event.
+     *
+     * @return The event end time.
+     */
     Time getEventEndTime();
+
+    /**
+     * Sets the end time of the event.
+     *
+     * @param eventEndTime The event end time to set.
+     */
     void setEventEndTime(Time eventEndTime);
 
+    /**
+     * Gets the price of the event tickets.
+     *
+     * @return The event price.
+     */
     float getEventPrice();
+
+    /**
+     * Sets the price of the event tickets.
+     *
+     * @param eventPrice The event price to set.
+     */
     void setEventPrice(float eventPrice);
 
+    /**
+     * Gets the ID of the venue where the event is held.
+     *
+     * @return The venue ID.
+     */
     int getVenueID();
+
+    /**
+     * Sets the ID of the venue where the event is held.
+     *
+     * @param venueID The venue ID to set.
+     */
     void setVenueID(int venueID);
 
+    /**
+     * Gets the name of the venue where the event is held.
+     *
+     * @return The venue name.
+     */
     String getVenueName();
+
+    /**
+     * Sets the name of the venue where the event is held.
+     *
+     * @param venueName The venue name to set.
+     */
     void setVenueName(String venueName);
 
     // A setter for SeatingConfigID isn't provided as it is automatically assigned within the Event's constructor
--- src/main/java/com/teamoneboxoffice/interfaces/JARServiceImpl/operations/ISeatingConfig.java
+++ src/main/java/com/teamoneboxoffice/interfaces/JARServiceImpl/operations/ISeatingConfig.java
@@ -6,25 +6,61 @@ import com.teamoneboxoffice.entities.Section;
 
 import java.util.ArrayList;
 
+/**
+ * Interface for managing seating configurations and seat properties within a venue
+ * or event.
+ */
 public interface ISeatingConfig {
 
+    /**
+     * Retrieves the full SeatingConfig object based on its ID.
+     *
+     * @param seatingConfigID The ID of the seating configuration to retrieve.
+     * @return The SeatingConfig object, or null if not found.
+     */
+    SeatingConfig getSeatingConfiguration(int seatingConfigID);
 
-  // Get the full SeatingConfig object (if needed)
-  SeatingConfig getSeatingConfiguration(int seatingConfigID);
+    // Get and set section names - (Commented out in the original code, assuming
+    // these were placeholders or not needed)
 
-  // Get and set section names
+    /**
+     * Retrieves a list of all seats within a specific section.
+     *
+     * @param section The Section object to retrieve seats from.
+     * @return An ArrayList of Seat objects in the specified section.
+     */
+    ArrayList<Seat> getSeats(Section section);
 
+    /**
+     * Retrieves a list of restricted view seats within a specific section.
+     *
+     * @param section The Section object to retrieve restricted seats from.
+     * @return An ArrayList of restricted view Seat objects in the specified section.
+     */
+    ArrayList<Seat> getRestrictedSeats(Section section);
 
-  // Retrieve seats from a section
-  ArrayList<Seat> getSeats(Section section);
+    /**
+     * Sets the booked status of a specific seat.
+     *
+     * @param seat   The Seat object to update.
+     * @param booked The booked status to set (true if booked, false if available).
+     */
+    void setSeatBooked(Seat seat, boolean booked);
 
-  ArrayList<Seat> getRestrictedSeats(Section section);
+    /**
+     * Sets the VIP status of a specific seat.
+     *
+     * @param seat The Seat object to update.
+     * @param vip  The VIP status to set (true if VIP, false otherwise).
+     */
+    void setSeatVip(Seat seat, boolean vip);
 
-  // Modify seat status
-  void setSeatBooked(Seat seat, boolean booked);
-
-  void setSeatVip(Seat seat, boolean vip);
-
-  void setSeatRestrictedView(Seat seat, boolean restricted);
+    /**
+     * Sets the restricted view status of a specific seat.
+     *
+     * @param seat       The Seat object to update.
+     * @param restricted The restricted view status to set (true if restricted view,
+     *                   false otherwise).
+     */
+    void setSeatRestrictedView(Seat seat, boolean restricted);
 }
-
